83d4ce7abd9f2c755922d37c14b15a3532e47d9e
danielkwon89 <danielkwon89@gmail.com>
2020-10-20 09:58:02 -0400

    Added actions to GeniusesController.

--- app/controllers/geniuses_controller.rb
+++ app/controllers/geniuses_controller.rb
@@ -1,2 +1,31 @@
-class GeniusesController < ApplicationController
+class GeniusesController < ApplicationController  
+    def new
+        @genius = Genius.new
+    end
+
+    def create
+        if genius_params[:password] == genius_params[:password_confirmation] && !Genius.find_by(email: genius_params[:email]) && !Angel.find_by(email: genius_params[:email])
+            Genius.create(first_name: genius_params[:first_name], last_name: genius_params[:last_name], email: genius_params[:email], password: genius_params[:password])
+
+            redirect_to 'pitches/index'
+
+        elsif genius_params[:password] != genius_params[:password_confirmation]
+            flash[:alert] = "Passwords don't match."
+            render :new
+
+        else
+            flash[:alert] = "Email already exists."
+            render :new
+        end
+    end
+
+    def show
+        @genius = Genius.find(params[:id])
+    end
+
+    private
+
+    def genius_params
+        params.require(:genius).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end

81e702ce967e89ce794e265d9125a39e7f4b3900
danielkwon89 <danielkwon89@gmail.com>
2020-10-20 17:20:25 -0400

    Created new pitch form. Created pitches#create action.

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -1,2 +1,26 @@
 class PitchesController < ApplicationController
+    def index
+        @pitches = Pitch.all
+    end
+
+    def show
+        @pitch = Pitch.find(params[:id])
+    end
+
+    def new
+        @pitch = Pitch.new
+    end
+
+    def create
+        # binding.pry
+        Pitch.create(title: pitch_params[:title], description: pitch_params[:description], category: pitch_params[:category], funding_start_date: pitch_params[:funding_start_date].to_datetime, funding_end_date: pitch_params[:funding_end_date].to_datetime, total_funding: pitch_params[:total_funding].to_f, funding_goal: pitch_params[:funding_goal].to_f, genius_id: session[:genius_id])
+        
+        redirect_to pitches_path
+    end
+
+    private
+
+    def pitch_params
+        params.require(:pitch).permit(:title, :description, :category, :funding_start_date, :funding_end_date, :total_funding, :funding_goal)
+    end
 end

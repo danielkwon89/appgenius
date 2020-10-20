70e0ab9ccaa4f59d4f898fe7b5ce763cd3d64a0b
danielkwon89 <danielkwon89@gmail.com>
2020-10-20 09:57:12 -0400

    Added actions to AngelsController.

--- app/controllers/angels_controller.rb
+++ app/controllers/angels_controller.rb
@@ -1,2 +1,30 @@
 class AngelsController < ApplicationController
+    def new
+        @angel = Angel.new
+    end
+
+    def create
+        if angel_params[:password] == angel_params[:password_confirmation] && !Genius.find_by(email: angel_params[:email]) && !Angel.find_by(email: angel_params[:email])
+            Angel.create(first_name: angel_params[:first_name], last_name: angel_params[:last_name], email: angel_params[:email], password: angel_params[:password])
+
+            redirect_to 'pitches/index'
+        elsif angel_params[:password] != angel_params[:password_confirmation]
+            flash[:alert] = "Passwords don't match."
+            render :new
+
+        else
+            flash[:alert] = "Email already exists."
+            render :new
+        end
+    end
+
+    def show
+        @angel = Angel.find(params[:id])
+    end
+
+    private
+
+    def angel_params
+        params.require(:angel).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
